c50f33fdb71a6516ab596fdfb8b79b074f9f8773
Luc Patiny <luc@patiny.com>
2020-11-02 16:13:32 +0100

    chore: add failing testcase

--- src/util/__tests__/antiLowerConvexHull.test.js
+++ src/util/__tests__/antiLowerConvexHull.test.js
@@ -8,11 +8,10 @@ describe('testing lower convexhull function', () => {
     expect(lowerConvexHull).toStrictEqual([0, 6, 5]);
   });
 
-  it.only('Square', () => {
-    const x = [1, 1, 1, 2, 2, 2, 3, 3, 3];
-    const y = [1, 2, 3, 1, 2, 3, 1, 2, 3];
+  it('Get anti clockwise lower convex hull with point reduction', () => {
+    const x = [1, 1, 2, 3, 4, 3, 2];
+    const y = [2, 4, 7, 6, 4, 3, 0];
     const lowerConvexHull = antiLowerConvexHull(x, y);
-    console.log(lowerConvexHull);
     expect(lowerConvexHull).toStrictEqual([0, 6, 5]);
   });
 });
