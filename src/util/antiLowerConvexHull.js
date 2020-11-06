a47a64632bb66c6c2e77c6a67ac77dfa5baee443
josoriom <javier.osorio@correounivalle.edu.co>
2020-11-06 07:54:01 -0500

    remove result close #22

--- src/util/antiLowerConvexHull.js
+++ src/util/antiLowerConvexHull.js
@@ -1,7 +1,7 @@
 /**
  * Preparata, F. P., & Shamos, M. I. (2012). Computational geometry: an introduction. Springer Science & Business Media.
- * @param {x} x - The array with x coordinates of the points.
- * @param {x} y - The array with y coordinates of the points.
+ * @param {Array} x - The array with x coordinates of the points.
+ * @param {Array} y - The array with y coordinates of the points.
  * @return {Array} The indices of the points of anticlockwise lower convex hull
  */
 export default function antiLowerConvexHull(x, y) {
@@ -14,7 +14,6 @@ export default function antiLowerConvexHull(x, y) {
   if (nbPoints === 1) return [0, 1];
 
   let currentPoint = 0;
-  let result = new Array(nbPoints + 1).fill().map((value, index) => index);
   while (true) {
     const a = currentPoint;
     const b = moveOn(currentPoint, nbPoints, x);
@@ -30,13 +29,13 @@ export default function antiLowerConvexHull(x, y) {
     } else {
       x[b] = -1;
       y[b] = -1;
-      result[b] = -1;
       currentPoint = moveBack(currentPoint, nbPoints, x);
     }
     if (c === nbPoints) break;
   }
-  result = result.filter((item) => item !== -1);
-  return result;
+  return x
+    .map((item, index) => (item === -1 ? -1 : index))
+    .filter((item) => item >= 0);
 }
 
 /**
